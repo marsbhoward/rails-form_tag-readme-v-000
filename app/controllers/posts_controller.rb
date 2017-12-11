3435cc4110ce729ec191e135e61ad6cdba2307ac
Mars Howard <marsbhoward@gmail.com>
2017-12-11 16:59:53 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new 
+  end
+
+  def create 
+    Post.creaete(title: params[:post][:title], description: params [:post][:description])
   
 end
